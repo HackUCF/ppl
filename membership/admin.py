e42fc4a95f073a3ddd62301f07ffc41f0e269c91
Mark Ignacio <mark@ignacio.io>
2015-10-07 16:23:55 -0400

    more powerful admin panel

--- membership/admin.py
+++ membership/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from membership.models import Member
 
-# Register your models here.
-admin.site.register(Member)
+
+class MemberAdmin(admin.ModelAdmin):
+    search_fields = ('knights_email', 'preferred_email', 'name')
+    list_display = ('name', 'knights_email', 'preferred_email', 'paid_dues')
+    list_filter = ('paid_dues',)
+
+
+admin.site.register(Member, MemberAdmin)
