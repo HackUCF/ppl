c25366c94917b15d25a97ef6b9eff9a2759c02a9
Mark Ignacio <mark@ignacio.io>
2015-12-07 11:55:00 -0500

    change knights email search to knights + name search

--- membership/forms.py
+++ membership/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    knights_email = forms.CharField(required=True, min_length=2)
+    query = forms.CharField(required=True, min_length=2)
--- membership/views.py
+++ membership/views.py
@@ -1,10 +1,10 @@
 import json
 
 from django.conf import settings
-
 from django.contrib.auth.decorators import user_passes_test, login_required
-from django.core.serializers.json import DjangoJSONEncoder
 from django.core.cache import cache
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Q
 from django.forms import model_to_dict
 from django.http import HttpResponseBadRequest, HttpResponse, \
     HttpResponseForbidden
@@ -43,8 +43,11 @@ def search(request):
     if not form.is_valid():
         return HttpResponseBadRequest(content=form.errors)
 
-    knights_email = form.cleaned_data['knights_email']
-    members = Member.objects.filter(knights_email__contains=knights_email)[:20]
+    query = form.cleaned_data['query']
+    members = Member.objects.filter(
+        Q(knights_email__icontains=query) |
+        Q(name__icontains=query)
+    )[:40]
     data = json.dumps({
         'results': {
             'data': [model_to_dict(m) for m in members]
