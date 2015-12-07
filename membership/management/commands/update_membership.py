8fe6887f0cf15abc472487c21c5d1a7a4cdc8402
Mark Ignacio <mark@ignacio.io>
2015-12-07 11:50:50 -0500

    patch for oauth2client compatibility

--- membership/management/commands/update_membership.py
+++ membership/management/commands/update_membership.py
@@ -1,8 +1,14 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+
 from membership import api
 from membership.email import sync_mailchimp
-
 from membership.models import update_membership
+
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 
 class Command(BaseCommand):
@@ -19,11 +25,12 @@ class Command(BaseCommand):
                             help='Do not run a local web server.')
         parser.add_argument('--auth_host_port', default=[8080, 8090], type=int,
                             nargs='*', help='Port web server should listen on.')
+        parser.add_argument('--logging-level', default='ERROR')
 
     def handle(self, *args, **options):
         output_file = options['output_file']
         self.stdout.write('[+] Exporting membership to {}'.format(output_file))
-        api.download_membership_file(options, output_file)
+        api.download_membership_file(AttrDict(options), output_file)
         self.stdout.write('[+] Updating membership database...')
         members = update_membership(output_file)
         if options['mailchimp_api_key']:
@@ -31,4 +38,3 @@ class Command(BaseCommand):
             added = sync_mailchimp(options['mailchimp_api_key'], members)
             print('[+] Added {} subscribers (need confirmation)'.format(
                 len(added)))
-
